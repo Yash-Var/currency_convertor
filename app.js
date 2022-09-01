19dc7075c058939c087292c8cdaf0683899e1664
Yash Varshney <yashvarshneypepsi@gmail.com>
2022-09-01 19:10:07 +0530

    country code

---
+++ app.js
@@ -0,0 +1,7 @@
+const dropList=document.querySelectorAll('.drop-list');
+
+for(let i=0;i<dropList.length;i++){
+    for(currency_code in country_code){
+        console.log(currency_code);
+    }
+}
